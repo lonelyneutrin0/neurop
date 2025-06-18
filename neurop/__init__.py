79417c465899147340a81394de76fc74b17ee80d
Aditya Narayan <adityanarayan2609@gmail.com>
2025-06-18 10:32:58 +0530

    Authorship update- See #5

--- neurop/__init__.py
+++ neurop/__init__.py
@@ -1,9 +1,11 @@
 __version__ = "0.0.3"
 __authors__ = [
-    "Hrishikesh Belagali"
+    "Hrishikesh Belagali",
+    "Aditya Narayan"
 ]
 __author_emails__ = [
     "belagal1@msu.edu",
+    "ma24btech11001@iith.ac.in"
 ]
 __url__ = "https://github.com/lonelyneutrin0/neurop"
 
