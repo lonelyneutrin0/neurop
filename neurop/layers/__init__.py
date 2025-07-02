bd833112850f191b692fe47807e8322f26552ec1
Hrishikesh Belagali <belagal1@msu.edu>
2025-07-01 23:22:10 -0400

    Update __init__ methods

--- neurop/layers/__init__.py
+++ neurop/layers/__init__.py
@@ -1,10 +1,2 @@
 from .iolayers import ReadinLayer, ReadoutLayer
 from .spectralconv import SpectralConv1DLayer, SpectralConv2DLayer, SpectralConv3DLayer
-
-__all__ = [ 
-    "ReadinLayer",
-    "ReadoutLayer",
-    "SpectralConv1DLayer",
-    "SpectralConv2DLayer",
-    "SpectralConv3DLayer",
-] # type: ignore
