f5f1ce969c5847c299cc261148689086cf858904
Hrishikesh Belagali <belagal1@msu.edu>
2025-07-08 18:47:00 -0400

    FNOs with skip connections

--- neurop/operators/fourier.py
+++ neurop/operators/fourier.py
@@ -1,105 +1,73 @@
 from torch.types import Tensor
 from typing import List, Union, Literal, Type
-
+from ..layers.skip_connections import Connection, ConnectionType
 import torch
 
 from ..base import NeuralOperator
 from ..layers.io_layers import ReadinLayer, ReadoutLayer
-from ..layers.spectral_convolution import SpectralConv1DLayer, SpectralConv2DLayer, SpectralConv3DLayer, SpectralConvNDLayer
-
-Connection = Literal['identity', 'linear', 'soft-gating']
+from ..layers.spectral_convolution import SpectralConv
+from ..layers.fno_unit import FNOUnit
 class FourierOperator(NeuralOperator):
+    """ 
+    Fourier Neural Operator (FNO) for learning mappings between functions in the Fourier domain.
+    This operator uses spectral convolutions to learn the mapping between input and output functions.
     """
-    Fourier Neural Operator. 
-    It learns a spectral kernel that can be used to approximate functions in the frequency domain.
-    """
-    
-    readin: ReadinLayer
-    """Layer that reads in input data and projects it to a higher dimensional space."""
 
-    kernel_integral: torch.nn.ModuleList
-    """List of spectral convolution layers that apply Fourier transforms to the input data."""
-
-    readout: ReadoutLayer
-    """Layer that reads out data to a lower dimensional space."""
-
-    depth: int
-    """Depth of the operator, i.e., number of spectral convolution layers."""
-
-    activation_function: Type[torch.nn.Module]
-    """Activation function to apply after each layer."""
-
-    skip_connection: Connection
-    """Skip connection type - identity, linear, soft-gating"""
-
-    def __init__(self, 
-                 in_features: int, 
-                 hidden_features: int, 
+    def __init__(self,
+                 in_features: int,
+                 hidden_features: int,
                  out_features: int,
-                 modes: Union[int, List], 
-                 depth: int = 3,
-                 *,
-                 activation_function: Type[torch.nn.Module] = torch.nn.ReLU,
-                 skip_connection: Connection = 'soft-gating'
-                 ):
+                 modes: Union[int, List[int]],
+                 n_dim: int,
+                 depth: int = 4,
+                 activation_function: torch.nn.Module = torch.nn.ReLU(),
+                 conv_modules: Type[SpectralConv] = SpectralConv,
+                 skip_connections: Union[ConnectionType, List[ConnectionType]] = 'soft-gating',
+                 bias: bool = True,
+                 init_scale: float = 1.0,
+                 dtype: torch.dtype = torch.cfloat):
         """
-        Initializes the Fourier operator with the given parameters.
-
-        Args:
-            in_features (int): Number of input features.
-            hidden_features (int): Number of hidden features.
-            out_features (int): Number of output features.
-            modes (Union[int, List]): Number of Fourier modes to consider. 
-                                       If a list, it should contain the number of modes for each dimension.
-            depth (int): Depth of the operator, i.e., number of spectral convolution layers.
-            activation_function (Callable[[Tensor], Tensor]): Activation function to apply after each layer.
-        
-        Returns:
-            None
+        Initializes the FourierOperator with the given parameters.
         """
         super().__init__()
-        self.readin = ReadinLayer(in_features, hidden_features)
 
-        if isinstance(modes, int):
-            self.kernel_integral = torch.nn.ModuleList([
-                SpectralConv1DLayer(hidden_features, hidden_features, modes = modes) for _ in range(depth)
-             ])
-        elif len(modes) == 2:
-            self.kernel_integral = torch.nn.ModuleList([
-                SpectralConv2DLayer(hidden_features, hidden_features, mode_h = modes[0], mode_w = modes[1]) for _ in range(depth)
-            ])
-        elif len(modes) == 3:
-            self.kernel_integral = torch.nn.ModuleList([
-                SpectralConv3DLayer(hidden_features, hidden_features, mode_d = modes[0], mode_h = modes[1], mode_w = modes[2]) for _ in range(depth)
-            ])
-        
-        else:
-            self.kernel_integral = torch.nn.ModuleList([
-                SpectralConvNDLayer(hidden_features, hidden_features, modes = modes) for _ in range(depth)
-            ])
+        self.readin = ReadinLayer(in_features=in_features, hidden_features=hidden_features)
+
+        self.fno_units = torch.nn.ModuleList(
+            [
+                FNOUnit(
+                    in_features=hidden_features, 
+                    out_features=hidden_features,  
+                    modes=modes,
+                    n_dim=n_dim, 
+                    activation_function=activation_function,
+                    conv_module=conv_modules,
+                    skip_connection=skip_connections[i] if isinstance(skip_connections, list) else skip_connections,
+                    bias=bias,
+                    init_scale=init_scale,
+                    dtype=dtype
+                )
+                for i in range(depth)
+            ]
+        )
 
         self.readout = ReadoutLayer(hidden_features, out_features)
-        self.depth = depth
-        self.activation_function = activation_function
-        self.skip_connection = skip_connection
 
-    def forward(self, x) -> Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         """
-        Forward pass for the Fourier operator.
+        Forward pass of the Fourier Operator.
         
         Args:
-            x (Tensor): Input tensor.
+            x (Tensor): Input tensor of shape (B, in_features, *spatial_dims).
         
         Returns:
-            Tensor: Tensor after applying the Fourier operator.
+            Tensor: Output tensor of shape (B, out_features, *spatial_dims).
         """
         x = self.readin(x)
-        x = self.activation_function(x)
-
-        for layer in self.kernel_integral:
-            x = layer(x)
-            
-            x = self.activation_function(x)
-
+        
+        for fno_unit in self.fno_units:
+            x = fno_unit(x)
+        
         x = self.readout(x)
+        
         return x
